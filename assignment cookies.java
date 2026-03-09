b5c6b74a446d5dfd43666cd224865e55d3a0eadc
nandithalm <nandithalm00@gmail.com>
2026-03-09 23:14:03 +0530

    Create assignment cookies.java

---
+++ assignment cookies.java
@@ -0,0 +1,25 @@
+//leetcode assignment cookies(greedy)
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int n = g.length;
+        int m = s.length;
+        int i = 0;
+        int j = 0;
+
+
+        while(i < n && j < m){
+            if(s[j]>=g[i]){
+                i++;
+
+            }
+            j++; 
+                 
+            
+        }
+        return i;
+        
+    }
+    
+}
