162720873286bb43f9f834f712f62508756a8589
nandithalm <nandithalm00@gmail.com>
2026-03-14 06:45:55 +0530

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,3 +1,24 @@
+//recursion
+
+import java.util.*;
+
+public class Main {
+
+    static int fib(int n){
+        if(n <= 1)
+            return n;
+
+        return fib(n-1) + fib(n-2);
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(fib(n));
+    }
+}
+
 //memoization
 import java.util.*;
 
@@ -25,5 +46,51 @@ public class Main {
         System.out.println(f(n, dp));
 
         sc.close();
+    }
+}
+
+//tabulation
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] dp = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        System.out.println(dp[n]);
+    }
+}
+
+//Space optimixed DP
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int prev2 = 0;
+        int prev = 1;
+
+        for(int i=2;i<=n;i++){
+            int cur = prev + prev2;
+            prev2 = prev;
+            prev = cur;
+        }
+
+        System.out.println(prev);
     }
 }
