7bc3adf486017ce42b095f48ca2831c474a88876
nandithalm <nandithalm00@gmail.com>
2026-07-17 16:11:29 +0530

    Create Trie Implementation.java

---
+++ Trie Implementation.java
@@ -0,0 +1,6 @@
+//theory
+Implement Trie Data Structure to support these operations
+
+* insert(word) - to insert a string "word" in Trie
+* search(word) - to check if string "word" is present in Trie or not.
+* startsWith(word) - to check if there is any string in the Trie that starts with the given prefix string "word".
